50641a136ccdf0a3bf9222875a49a735d2c1e01d
andreea.moraru <andreeamoraru090@gmail.com>
2021-02-03 16:32:43 +0100

    basis documentation template in index.html

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 @app.route("/greet")
 def greet():
     username = request.args.get('name')
-    return render_template('index.html', name=username)
+    module_name = "Building the App’s Container Image"
+    paragraph = "In order to build the application, we need to use a Dockerfile. A Dockerfile is simply a text-based " \
+                "script of instructions that is used to create a container image. If you’ve created Dockerfiles " \
+                "before, you might see a few flaws in the Dockerfile below. But, don’t worry! We’ll go over them. "
+
+    code = "RUN yarn install --production"
+
+    return render_template('index.html', module_name=module_name, paragraph= paragraph, code= code)
 
 if __name__ == "__main__":
     app.run()
