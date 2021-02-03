6479374c9bea401a6831431ddd7b2f07c85032e8
andreea.moraru <andreeamoraru090@gmail.com>
2021-02-03 09:29:55 +0100

    basic template structure

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/greet")
+def greet():
+    username = request.args.get('name')
+    return render_template('index.html', name=username)
+
+if __name__ == "__main__":
+    app.run()
+
+
