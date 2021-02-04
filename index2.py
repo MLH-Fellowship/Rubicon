ea5bb18a74648e2859e9edb7f57028b1f259ec78
Anastasiya <anastasiyauraleva@gmail.com>
2021-02-04 01:41:50 -0500

    fixed tables and links to be in jinja

--- index2.py
+++ index2.py
@@ -12,29 +12,32 @@ def greet():
                 "script of instructions that is used to create a container image. If you’ve created Dockerfiles " \
                 "before, you might see a few flaws in the Dockerfile below. But, don’t worry! We’ll go over them. "
 
-    bullet_point=["Installing the Github integration for Slack", "Getting Started", "Need help?"]
     paragraph2= "When you install the new GitHub integration for Slack in your Slack workspace, you'll be prompted to move over all of your existing subscriptions - so getting set up again is easy. As you enable individual subscriptions in the new app, your settings will be automatically migrated and subscriptions in the legacy app will be disabled."
 
     #table and table list should be all stored in a dictionary in the future
     table_title = ['Event', 'Is Filtered']
-    # table_list = {
-    #     'Pull':'✅ Yes',
-    #     'Deployment': '❌ No',
-    # }
 
-    code = "RUN yarn install --production"
+    table_list=[
+        {'name': 'Pull', 'status': '✅ Yes'},
+        {'name': 'Deployment', 'status': '❌ No'}
+    ]
+    bullet_point = [
+        {'name': 'Getting Started', 'url': 'https://github.com/integrations/slack#requirements'},
+        {'name': 'Installing Github for Slack', 'url': 'https://github.com/integrations/slack#installation'},
+        {'name': 'Need help?', 'url': 'https://github.com/integrations/slack#questions-need-help'}
+    ]
 
     return render_template(
         'index2.html',
         paragraph= paragraph,
-        code= code,
         browser_title=browser_title,
         sub_title=sub_title,
         main_title=main_title,
         image_about=image_about,
         bullet_point=bullet_point,
         table_title=table_title,
-        paragraph2=paragraph2
+        paragraph2=paragraph2,
+        table_list=table_list
     )
 
 if __name__ == "__main__":
