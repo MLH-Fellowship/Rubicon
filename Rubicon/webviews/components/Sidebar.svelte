5784adc3d1fe38281ffce66e71caa3789d3ca93d
Anastasiya <anastasiyauraleva@gmail.com>
2021-02-06 23:00:49 -0500

    setup folder for express and set up code for messages

--- Rubicon/webviews/components/Sidebar.svelte
+++ Rubicon/webviews/components/Sidebar.svelte
@@ -1,9 +1,46 @@
-<script lang="ts"></script>
+<!--<script lang="ts" ✂prettier:content✂="CiAgbGV0IGRvY3MgPSBbXTsKICBsZXQgY291bnQgPSAwOwo=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=" ✂prettier:content✂="e30=">{}</script><div>{count}</div>
+<button
+  on:click={() => {
+    count++;
+  }}
+  >increment
+</button>
+<input /><style ✂prettier:content✂="CiAgZGl2IHsKICAgIGNvbG9yOiBwaW5rOwogIH0K77+977+977+977+9J++/vXLvv73vv716ew==" ✂prettier:content✂="" ✂prettier:content✂="" ✂prettier:content✂="" ✂prettier:content✂="" ✂prettier:content✂=""></style>-->
+<script lan="ts">
+  // let todos: Array<{ text: string; completed: boolean }> = [];
+  // let text = "";
+  // onMount(() => {
+  //   // Handle messages sent from the extension to the webview
+  //   window.addEventListener("message", (event) => {
+  //     const message = event.data; // The json data that the extension sent
+  //     console.log({ message });
+  //     switch (message.type) {
+  //       case "new-doc":
+  //         newDoc = [{ text: message.value, completed: false }, ...todos];
+  //         break;
+  //     }
+  //   });
+  // });
+</script>
 
-<div>Hello</div>
+<button
+  on:click={() => {
+    tsvscode.postMessage({
+      type: "onInfo",
+      value: "info message ",
+    });
+  }}
+>
+  click me</button
+>
 
-<style>
-  div {
-    background-color: pink;
-  }
-</style>
+<button
+  on:click={() => {
+    tsvscode.postMessage({
+      type: "onError",
+      value: "error message ",
+    });
+  }}
+>
+  error
+</button>
