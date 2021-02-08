7d63f34c6691334d6a94ac2e534098b6db017e42
andreea.moraru <andreeamoraru090@gmail.com>
2021-02-08 09:45:21 +0100

    edited the landing page, added the text and positioned the elements  in a different way.

--- utilities/request.js
+++ utilities/request.js
@@ -14,35 +14,35 @@ function convert_html_to_readme(html_code) {
 	});
 
 }
+// var propertiesObject = {
+// 	module_name : 'Building the App’s Container Image',
+// 	paragraph   : "In order to build the application, we need to use a Dockerfile. A Dockerfile is simply a text-based script of instructions that is used to create a container image. If you’ve created Dockerfiles ,before, you might see a few flaws in the Dockerfile below. But, don’t worry! We’ll go over them. ",
+// 	code :"RUN yarn install --production"
+//
+// };
+
+function request_jinja_template(propertiesObject) {
+	var request = require('request');
+	var fs = require('fs');
+	var url = 'http://127.0.0.1:5000/greet';
+	var html_page;
+
+	request({url:url, qs:propertiesObject}, function(err, response, body) {
+  		if(err) { console.log(err); return; }
+
+  		html_page = response.body;
 
 
-var request = require('request');
-var fs = require('fs');
+		fs.writeFile('example.html', html_page, function (err) {
+  			if (err) return console.log(err);
+  			console.log('the content s saved to file example.html');
+		});
 
-var propertiesObject = { 
-	module_name : 'Building the App’s Container Image',
-	paragraph   : "In order to build the application, we need to use a Dockerfile. A Dockerfile is simply a text-based script of instructions that is used to create a container image. If you’ve created Dockerfiles ,before, you might see a few flaws in the Dockerfile below. But, don’t worry! We’ll go over them. ",
-	code :"RUN yarn install --production" 
+		convert_html_to_readme(html_page)
 
-};
-
-var url = 'http://127.0.0.1:5000/greet';
-var html_page;
-
-request({url:url, qs:propertiesObject}, function(err, response, body) {
-  if(err) { console.log(err); return; }
-
-  html_page = response.body;
-
-
-	fs.writeFile('example.html', html_page, function (err) {
-  		if (err) return console.log(err);
-  		console.log('the content s saved to file example.html');
 	});
+}
 
-	convert_html_to_readme(html_page)
-
-});
 
 
 
